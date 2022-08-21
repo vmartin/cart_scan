e29b0370be57e592b385ab1130814471fc5aeef4
victor <victor.martin.romo@gmail.com>
2022-08-21 17:30:45 +0200

    Improve BaseRule test

--- test/spec/base_rule_spec.rb
+++ test/spec/base_rule_spec.rb
@@ -2,10 +2,15 @@ require_relative '../../lib/base_rule'
 require 'bigdecimal'
  
 describe BaseRule do
-  it "should get execution value 0.0£ when number of items items is 0 and price are 0£" do
+  it "should get execution value 0.0£ when number of items items is 0 or price are 0£" do
+    
     number_of_items = 1
     base_price = BigDecimal(0)   
-    expect(BaseRule.new.execute(number_of_items, base_price)).to eq(0)
+    expect(BaseRule.new.execute(number_of_items, base_price)).to eq(0)  
+
+    number_of_items = 0
+    base_price = BigDecimal(1)
+    expect(BaseRule.new.execute(number_of_items, base_price)).to eq(0)  
   end
 
   it "should get execution value 5.0£ when number of items is 1 and price is 5£" do
