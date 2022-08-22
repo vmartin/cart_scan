3c395bc798e37770a4c03ab0b52157236ad40940
victor <victor.martin.romo@gmail.com>
2022-08-22 17:26:51 +0200

    change spec descriptions on spec tests of mock_products test class

--- test/spec/mock_products_spec.rb
+++ test/spec/mock_products_spec.rb
@@ -7,15 +7,15 @@ describe MockProducts do
     expect(MockProducts.get_price("INVENTED_CODE")).to eq(0)  
   end
 
-  it "should get price of 0.0£ when product code is GR1" do   
+  it "should get price of 3.11£ when product code is GR1" do   
     expect(MockProducts.get_price("GR1")).to eq(3.11)  
   end
 
-  it "should get price of 0.0£ when product code is SR1" do   
+  it "should get price of 5.0£ when product code is SR1" do   
     expect(MockProducts.get_price("SR1")).to eq(5.00)  
   end 
 
-  it "should get price of 0.0£ when product code CF1" do    
+  it "should get price of 11.23£ when product code CF1" do    
     expect(MockProducts.get_price("CF1")).to eq(11.23)  
   end 
 
